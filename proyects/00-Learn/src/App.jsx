860277e6c75dcfecd9bc7a4aded7478b99d9c05f
Carlos Benitez <damianbenitez8928@gmail.com>
2024-04-01 13:29:07 -0300

    fix proyecto y nuevo componente

---
+++ proyects/00-Learn/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MiBoton from './components/MiBoton'
+
+
+function App() {
+  let texto ="lorem"
+  return (
+    <>
+      <MiBoton texto={texto}/>
+    </>
+  )
+}
+
+export default App
